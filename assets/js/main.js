66d2ffffb51fb5345f992acc4fe9a0a3ca0d2957
Mashiur Rahman <mashiur.dev@gmail.com>
2023-10-10 01:36:11 +0600

    loading updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,14 +13,16 @@ texts.forEach((text, index) => {
     setTimeout(function () {
       
         rotator.innerText = text;
-        if (index < texts.length-1) {
-            loader.style.width = 20 * index + "%";
-            console.log(index);
-        } else {
-            loader.style.width = "100%";
+        if (index > 0) {
+            if (index < texts.length - 1) {
+              loader.style.width = 30 * index + "%";
+            } else {
+              loader.style.width = "100%";
 
-            window.location.href = "http://yahoo.com";
+              window.location.replace("http://yahoo.com");
+            }
         }
+        
       
     }, index * 1000);
 
