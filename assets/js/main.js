e4a057f19cf2451b1271071155d0aaea6244d545
Mashiur Rahman <mashiur.dev@gmail.com>
2023-10-10 01:27:36 +0600

    redirect implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,14 @@ texts.forEach((text, index) => {
     setTimeout(function () {
       
         rotator.innerText = text;
-        //loader.style.width = 20 * index + '%';
-        console.log(20 * index);
+        if (index < texts.length-1) {
+            loader.style.width = 20 * index + "%";
+            console.log(index);
+        } else {
+            loader.style.width = "100%";
+
+            window.location.href = "http://yahoo.com";
+        }
       
     }, index * 1000);
 
